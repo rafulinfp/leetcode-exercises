96aef2af0c95759488ef61e39e7feb1eb36ef5aa
Rafael Fernandez <rafulinfp@gmail.com>
2020-09-01 16:11:53 -0700

    feat: added implemtation for 1. TwoSum

--- Program.cs
+++ Program.cs
@@ -15,6 +15,18 @@ namespace LeetCodeProblems
 
                 switch (problem)
                 {
+                    case "1":
+                        {
+                            var nums = new int[] { 2, 7, 11, 15 };
+                            Console.WriteLine($"Input: {String.Join(",", nums)} - Result: {String.Join(",", TwoSum1.TwoSum(nums, 9))}");
+
+                            var nums2 = new int[] { 3, 2, 4 };
+                            Console.WriteLine($"Input: {String.Join(",", nums)} - Result: {String.Join(",", TwoSum1.TwoSum(nums2, 6))}");
+
+                            var nums3 = new int[] { 3, 2, 3 };
+                            Console.WriteLine($"Input: {String.Join(",", nums)} - Result: {String.Join(",", TwoSum1.TwoSum(nums3, 6))}");
+                        }
+                        break;
                     case "242":
                         {
                             Console.WriteLine($"Input: s = \"rat\", t = \"car\" - Result: {ValidAnagram242.IsAnagram("rat", "car")}");
